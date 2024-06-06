485614d755755b618dab73752140be7353ee5587
Dat Nguyen <ddatnguyen97@gmail.com>
2024-06-06 09:52:18 +0700

    Add new queries

--- Airlines Reviews.sql
+++ Airlines Reviews.sql
@@ -3,7 +3,7 @@ select * from airlines_reviews;
 select "Airline Name", count(*) as  "Review Count"
 from airlines_reviews
 group by "Airline Name"
-order by "Review Count" DESC;
+order by "Review Count" desc;
 
 select "Airline Name", count(*) as "Positive Recommendations"
 from airlines_reviews
@@ -21,4 +21,15 @@ order by "Avg Overall Rating" desc;
 
 select "Customer Name", "Post Date", "Country", "Type Of Traveller", "Title", "Airline Name", "Recommended"
 from airlines_reviews
-WHERE EXTRACT(YEAR FROM TO_DATE("Post Date", 'YYYY-MM-DD')) = 2023;
+where extract(year from TO_DATE("Post Date", 'YYYY-MM-DD')) = 2023;
+
+select "Airline Name", avg("Ground Service") as "AVG Ground Service", 
+	avg("Seat Comfort") as "AVG Seat Comfort", 
+	avg("Cabin Staff Service") as "AVG Cabin Staff Service", 
+	avg("Food & Beverages") as "AVG F&B", 
+	avg("Inflight Entertainment") as " AVG Inflight Entertainment", 
+	avg("Wifi & Connectivity") as "AVG Wifi & Connectivity", 
+	avg("Value For Money") as "AVG Value For Money"
+from airlines_reviews
+group by "Airline Name"
+order by "Airline Name" desc
